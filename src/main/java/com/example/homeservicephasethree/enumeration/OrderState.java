62c7ac897293803cad2a2db31b41754c4082a434
pc <mahdierasooli693@gamil.com>
2023-08-30 13:00:03 +0330

    creating an enum for order to handle the state of the order

---
+++ src/main/java/com/example/homeservicephasethree/enumeration/OrderState.java
@@ -0,0 +1,10 @@
+package com.example.homeservicephasethree.enumeration;
+
+public enum OrderState {
+    WAITING_EXPERT_SUGGESTION,
+    WAITING_FOR_CHOOSING_EXPERT,
+    WAITING_FOR_EXPERT_TO_COME,
+    STARTED,
+    DONE,
+    PAID
+}
