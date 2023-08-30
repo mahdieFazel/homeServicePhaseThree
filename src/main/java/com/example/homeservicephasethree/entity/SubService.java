260036e34509623a4add278f2ab8e8936b7927d8
pc <mahdierasooli693@gamil.com>
2023-08-30 18:35:10 +0330

    completing SubService entity

--- src/main/java/com/example/homeservicephasethree/entity/SubService.java
+++ src/main/java/com/example/homeservicephasethree/entity/SubService.java
@@ -4,9 +4,17 @@ import com.example.homeservicephasethree.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public class SubService extends BaseEntity<Long> {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+class SubService extends BaseEntity<Long> {
     @Column(unique = true)
     private String name;
     private Long basePrice;
