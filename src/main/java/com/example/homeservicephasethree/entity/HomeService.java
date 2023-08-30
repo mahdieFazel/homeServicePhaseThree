ac3689ec5ef65cac4fd90d63b9d03bade29d5793
pc <mahdierasooli693@gamil.com>
2023-08-30 18:23:59 +0330

    completing HomeService entity

--- src/main/java/com/example/homeservicephasethree/entity/HomeService.java
+++ src/main/java/com/example/homeservicephasethree/entity/HomeService.java
@@ -2,12 +2,19 @@ package com.example.homeservicephasethree.entity;
 
 import com.example.homeservicephasethree.base.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class HomeService extends BaseEntity<Long> {
     @Column(unique = true)
     private String name;
