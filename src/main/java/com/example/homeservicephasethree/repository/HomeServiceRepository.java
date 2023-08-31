0d4ab15456f1e8213740b2152b7e574e40a35234
pc <mahdierasooli693@gamil.com>
2023-08-31 18:59:00 +0330

    complete methods of this repository

--- src/main/java/com/example/homeservicephasethree/repository/HomeServiceRepository.java
+++ src/main/java/com/example/homeservicephasethree/repository/HomeServiceRepository.java
@@ -5,6 +5,11 @@ import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HomeServiceRepository extends JpaRepository<HomeService, Long> {
+    Optional<HomeService> findByName(String name);
+
+    int deleteHomeServiceById(Long HomeServiceId);
 }
