e4bf418d1c7ee50ee513cc6e122811e2a725332d
pc <mahdierasooli693@gamil.com>
2023-09-01 00:37:10 +0330

    Create OfferService

--- src/main/java/com/example/homeservicephasethree/service/OfferService.java
+++ src/main/java/com/example/homeservicephasethree/service/OfferService.java
@@ -1,6 +1,13 @@
 package com.example.homeservicephasethree.service;
 
-import com.example.homeservicephasethree.service.serviceImpl.OfferServiceImpl;
+import com.example.homeservicephasethree.base.BaseService;
+import com.example.homeservicephasethree.entity.Offer;
 
-public class OfferService implements OfferServiceImpl {
+import java.util.List;
+
+public interface OfferService extends BaseService<Offer, Long> {
+    List<Offer> findByOrderIdBasedOnProposedPrice(Long orderId);
+
+    List<Offer> findByOrderIdBasedOnExpertScore(Long orderId);
+
 }
--- src/main/java/com/example/homeservicephasethree/service/serviceImpl/OfferServiceImpl.java
+++ src/main/java/com/example/homeservicephasethree/service/serviceImpl/OfferServiceImpl.java
@@ -1,4 +1,38 @@
 package com.example.homeservicephasethree.service.serviceImpl;
 
-public interface OfferServiceImpl {
+import com.example.homeservicephasethree.base.BaseServiceImpl;
+import com.example.homeservicephasethree.entity.Offer;
+import com.example.homeservicephasethree.repository.OfferRepository;
+import com.example.homeservicephasethree.service.OfferService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+
+public class OfferServiceImpl
+        extends BaseServiceImpl<Offer, Long, OfferRepository> implements OfferService {
+
+    public OfferServiceImpl(OfferRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public List<Offer> findByOrderIdBasedOnProposedPrice(Long orderId) { try {
+        return repository.findByOrderIdBasedOnProposedPrice(orderId);
+    } catch (Exception e) {
+        return Collections.emptyList();
+    }
+    }
+
+    @Override
+    public List<Offer> findByOrderIdBasedOnExpertScore(Long orderId) {
+        try {
+            return repository.findByOrderIdBasedOnExpertScore(orderId);
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
+    }
 }
