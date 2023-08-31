ec54737bb2b2b0ecaaeb88f097d72ac59b8af3f4
pc <mahdierasooli693@gamil.com>
2023-09-01 00:37:26 +0330

    Create OrderService

--- src/main/java/com/example/homeservicephasethree/service/OrderService.java
+++ src/main/java/com/example/homeservicephasethree/service/OrderService.java
@@ -1,6 +1,17 @@
 package com.example.homeservicephasethree.service;
 
-import com.example.homeservicephasethree.service.serviceImpl.OrderServiceImpl;
 
-public class OrderService implements OrderServiceImpl {
+import com.example.homeservicephasethree.base.BaseService;
+import com.example.homeservicephasethree.entity.Order;
+import com.example.homeservicephasethree.enumeration.OrderState;
+
+import java.util.List;
+
+public interface OrderService extends BaseService<Order, Long> {
+    List<Order> findByCustomerId(Long customerId);
+
+    List<Order> findByCustomerIdAndOrderState(Long customerId, OrderState orderState);
+    List<Order> findBySubServiceIdAndOrderStatus(Long subServiceId, OrderState orderStateOne, OrderState orderStateTwo);
+    int changeOrderStatus(Long orderId, OrderState orderState, OrderState newOrderState);
+
 }
--- src/main/java/com/example/homeservicephasethree/service/serviceImpl/OrderServiceImpl.java
+++ src/main/java/com/example/homeservicephasethree/service/serviceImpl/OrderServiceImpl.java
@@ -1,5 +1,55 @@
 
 package com.example.homeservicephasethree.service.serviceImpl;
 
-public interface OrderServiceImpl {
+import com.example.homeservicephasethree.base.BaseServiceImpl;
+import com.example.homeservicephasethree.entity.Order;
+import com.example.homeservicephasethree.enumeration.OrderState;
+import com.example.homeservicephasethree.repository.OrderRepository;
+import com.example.homeservicephasethree.service.OrderService;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+@Service
+
+public class OrderServiceImpl
+        extends BaseServiceImpl<Order, Long, OrderRepository> implements OrderService {
+    private EntityManager entityManager;
+
+    public OrderServiceImpl(OrderRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public List<Order> findByCustomerId(Long customerId) {
+        try {
+            return repository.findByCustomerId(customerId);
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<Order> findByCustomerIdAndOrderState(Long customerId, OrderState orderStatus) {
+        try {
+            return repository.findByCustomerIdAndOrderStatus(customerId, orderStatus);
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public List<Order> findBySubServiceIdAndOrderStatus(Long subServiceId, OrderState orderStateOne, OrderState orderStateTwo) {
+        try {
+            return repository.findBySubServiceIdAndOrderStatus(subServiceId, orderStateOne, orderStateTwo);
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public int changeOrderStatus(Long orderId, OrderState orderState, OrderState newOrderState) {
+        return repository.changeOrderStatus(orderId, orderState, newOrderState);
+    }
 }
