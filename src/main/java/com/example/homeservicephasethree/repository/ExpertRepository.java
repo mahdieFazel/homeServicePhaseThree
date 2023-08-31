e690eb24177a4e4a56c6d71e76f225834b762ffa
pc <mahdierasooli693@gamil.com>
2023-08-31 18:58:29 +0330

    complete methods of this repository

--- src/main/java/com/example/homeservicephasethree/repository/ExpertRepository.java
+++ src/main/java/com/example/homeservicephasethree/repository/ExpertRepository.java
@@ -1,10 +1,38 @@
 package com.example.homeservicephasethree.repository;
 
 import com.example.homeservicephasethree.entity.Expert;
+import com.example.homeservicephasethree.enumeration.PersonStatus;
 import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
+    Optional<Expert> findByEmail(String email);
+
+    Optional<Expert> findByUsername(String phoneNumber);
+    @Query("""
+            update Expert e
+            set e.password = :newPassword
+            where e.id = :expertId
+            """)
+    int editPassword(Long expertId, String newPassword);
+    @Query("""
+            update Expert e
+            set e.personStatus = :expertStatus
+            where e.id = :expertId
+            """)
+    int changeExpertStatus(Long expertId, PersonStatus personStatus);
+
+    @Query("""
+            update Expert e
+            set e.isActive = :isActive
+            where e.id = :expertId
+            """)
+    int changeExpertActivation(Long expertId, Boolean isActive);
+
+
 }
