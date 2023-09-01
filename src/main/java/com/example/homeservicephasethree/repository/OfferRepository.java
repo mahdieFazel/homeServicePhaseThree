3399ef991a1c00f7cb0012592aca84e2a37773ca
pc <mahdierasooli693@gamil.com>
2023-09-01 06:54:34 +0330

    delete addition imports

--- src/main/java/com/example/homeservicephasethree/repository/OfferRepository.java
+++ src/main/java/com/example/homeservicephasethree/repository/OfferRepository.java
@@ -1,7 +1,6 @@
 package com.example.homeservicephasethree.repository;
 
 import com.example.homeservicephasethree.entity.Offer;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
